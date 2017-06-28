977c48c09b2f8e0a848f7ea4f8dc20b723c03ea6
Chris Vickery <chrisinajar@gmail.com>
2017-06-28 15:44:29 -0700

    Use spok for deep equals testing Closes: #1

--- test.js
+++ test.js
@@ -1,14 +1,14 @@
 var test = require('tape');
+var spok = require('spok');
 var Parse = require('./');
 var TestData = require('./tests/basic.json');
 
 test('fixture data', function (t) {
-  var numberOfTests = 1;
-  t.plan(TestData.length * numberOfTests);
-
   TestData.forEach(function (data) {
     var parser = Parse(data.schema);
     var result = parser(data.input);
-    t.deepEqual(result, data.expected);
+    spok(t, result, data.expected);
   });
+
+  t.end();
 });
